9d2c7cdda77092483128cfc6256411df3746a91c
owenburton <owenburton@users.noreply.github.com>
2020-04-08 13:16:37 -0700

    changed my_id & miner.py v1

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -13,8 +13,11 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
-
+    block_string = json.dumps(block, sort_keys=True)
+    proof = 0
+    while not valid_proof(block_string, proof):
+        proof += 1
+    return proof
 
 def valid_proof(block_string, proof):
     """
@@ -27,7 +30,11 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:3] == '000'
+    # return guess_hash[:6] == '000000'
+
 
 
 if __name__ == '__main__':
@@ -43,6 +50,9 @@ if __name__ == '__main__':
     print("ID is", id)
     f.close()
 
+    # Num block rewards received
+    coins = 0
+
     # Run forever until interrupted
     while True:
         r = requests.get(url=node + "/last_block")
@@ -56,7 +66,9 @@ if __name__ == '__main__':
             break
 
         # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        print('starting work..')
+        new_proof = proof_of_work(data)
+        print('work finished')
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
@@ -67,4 +79,16 @@ if __name__ == '__main__':
         # TODO: If the server responds with a 'message' 'New Block Forged'
         # add 1 to the number of coins mined and print it.  Otherwise,
         # print the message from the server.
-        pass
+        m = data['message']
+        if m != 'new block forged':
+            print(m)
+            break
+        
+        coins += 1
+        print('num coins: ', coins)
+
+        # mining limit
+        if coins>2:
+            break
+        
+
